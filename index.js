2c8e0833fb365a94642004afc8b35a7ae33177cc
Hannah <hrglasser94@gmail.com>
2018-07-31 15:54:59 -0400

    Removed local storage funcitonality completely.

--- index.js
+++ index.js
@@ -1,43 +1,24 @@
 var URL = "https://dc-coffeerun.herokuapp.com/api/coffeeorders";
-var globalOrders;
-function storeData(orders) {
+function displayData(orders) {
     localStorage.setItem('coffeeOrders', JSON.stringify(orders))
     for (var property in orders) {
         addStoredOrders(orders[property]);
     };
 }
-// var myData = {
-//     coffee: 'espresso', 
-//     emailAddress: 'hello@world.com',
-//     strength: 30,
-//     size: "tall",
-//     flavor: "vanilla"
-// }
-// $.post(URL, myData);
-$.get(URL, storeData);
 
+$.get(URL, displayData);
 
 var coffeeOrderForm = document.querySelector(".coffee-order-form");
 var deleteForm = document.querySelector(".remove-orders");
 var orderList = document.querySelector('.order-list');
 var orderListArray = [];
+var deleted;
 
 var addStoredOrders = function(orderObject) {
     var orderArray = [orderObject["size"], orderObject["coffee"], "with a shot of ", orderObject["flavor"], "and", orderObject["strength"], "milligrams of caffeine for ", orderObject["emailAddress"]]
     var order = document.createElement('li');
     order.setAttribute('class', 'order');
-    order.textContent = orderArray.join(" ");
-    var checkBox = document.createElement("input");
-    checkBox.setAttribute('type', 'checkbox');
-    checkBox.setAttribute('class', 'checkbox')
-    order.appendChild(checkBox);
-    orderList.appendChild(order);    
-}
-
-var addPendingOrderOnPage = function(orderObject) {
-    var orderArray = [orderObject["Size"], orderObject["Order"], "with a shot of ", orderObject["Flavor"], "and", orderObject["Caffeine"], "milligrams of caffeine for ", orderObject["Email"]]
-    var order = document.createElement('li');
-    order.setAttribute('class', 'order');
+    order.setAttribute('id', orderObject["emailAddress"]);
     order.textContent = orderArray.join(" ");
     var checkBox = document.createElement("input");
     checkBox.setAttribute('type', 'checkbox');
@@ -56,65 +37,35 @@ var createOrder = function() {
     checkBox.setAttribute('type', 'checkbox');
     checkBox.setAttribute('class', 'checkbox')
     var orderContent = {
-        "Order": coffeeOrder.value,
-        "Email": emailInput.value,
-        "Size": size.value, 
-        "Flavor": flavorShot.value,
-        "Caffeine": caffeineRating.value, 
-        "Checkbox": checkBox.checked, 
+        coffee: coffeeOrder.value,
+        emailAddress: emailInput.value,
+        size: size.value, 
+        flavor: flavorShot.value,
+        strength: caffeineRating.value, 
+        checkbox: checkBox.checked, 
     }
     orderListArray.push(orderContent);
-    addPendingOrderOnPage(orderContent);
-}
-
-// var compareOrderListArray = function() {
-//     var newArray = [];
-//     var orders = document.querySelectorAll('.order');
-//     console.log(orders);
-//     orderListArray.forEach(function(order) {
-//         if (orders.indexOf(order) > -1) {
-//             newArray.push(order);
-//         }
-//     })
-//     orderListArray = newArray;
-// }
-
-var getLocalStorage = function() {
-    var pending = localStorage.getItem('pending-orders');
-    var pendingArray = JSON.parse(pending);
-    if (pendingArray != null) {
-        pendingArray.forEach(function(order) {
-            addPendingOrderOnPage(order);
-        });
-    }
-}
-
-var updateOrderLists = function() {
-    localStorage.setItem('pending-orders', JSON.stringify(orderListArray));
+    addStoredOrders(orderContent);
+    $.post(URL, orderContent);
 }
 
 var handleSubmit = function(event) {
     event.preventDefault();
     createOrder();
-    updateOrderLists();
 }
 
 var deleteOrder = function(event) {
     event.preventDefault();
     checkBoxOrders = orderList.querySelectorAll('input[type=checkbox]:checked')
-    checkBoxOrders.forEach(function(element, position) {
+    checkBoxOrders.forEach(function(element) {
         parent = element.parentElement;
         if (parent.parentNode) {
             parent.parentNode.removeChild(parent);
+            var id = parent.getAttribute('id');
+            deleted = URL + "/" + id;
         }
     });
-    // updateOrderLists();
 }
-
-getLocalStorage();
 
 coffeeOrderForm.addEventListener('submit', handleSubmit);
 deleteForm.addEventListener('submit', deleteOrder);
-
-
-//$.ajax('https://dc-coffeerun.herokuapp.com/api/coffeeorders', 
